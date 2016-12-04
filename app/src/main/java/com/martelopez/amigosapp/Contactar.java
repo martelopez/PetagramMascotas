ea884adc7583da5ae31c0d75dbce94d7cf6d667a
emartelopez <emartelopez@gmail.com>
2016-12-04 12:35:24 -0500

    JavaMail

--- app/src/main/java/com/martelopez/amigosapp/Contactar.java
+++ app/src/main/java/com/martelopez/amigosapp/Contactar.java
@@ -1,6 +1,5 @@
 package com.martelopez.amigosapp;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,21 +9,28 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-public class Contactar extends AppCompatActivity implements View.OnClickListener {
+public class Contactar extends AppCompatActivity {
 
     //private TextInputLayout tilNombre, tilCorreo, tilDescripcion;
     private EditText etNombre, etCorreo, etDescripcion;
-    String nombre, correo, descripcion;
+
+    String nombre; String correo; String descripcion;
+
     Context context = null;
-    ProgressDialog pdialog = null;
+
     Button btnEnviar;
 
     String miCorreo, miClave;
@@ -42,7 +48,51 @@ public class Contactar extends AppCompatActivity implements View.OnClickListener
         etDescripcion = (EditText) findViewById(R.id.etDescripcion);
         btnEnviar = (Button) findViewById(R.id.btnEnviar);
 
-        btnEnviar.setOnClickListener(this);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nombre = etNombre.getText().toString();
+                correo = etCorreo.getText().toString();
+                descripcion = etDescripcion.getText().toString();
+
+                miCorreo = "martelopezclases@gmail.com";
+                miClave  = "Clases2016";
+
+                //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder.permitAll().build();
+                Properties properties = new Properties();
+                properties.put("mail.smtp.host", "smtp.gmail.com");
+                properties.put("mail.smtp.socketFactory.port", "465");
+                properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                properties.put("mail.smtp.auth", "true");
+                properties.put("mail.smtp.port", "465");
+
+                try {
+                    session = Session.getDefaultInstance(properties, new Authenticator() {
+                        @Override
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(miCorreo, miClave);
+                        }
+                    });
+
+                    if(session != null){
+
+                        Message message = new MimeMessage(session);
+                        message.setFrom(new InternetAddress("martelopezclases@gmail.com"));
+                        message.setSubject(nombre);
+
+                        message.setRecipient(Message.RecipientType.TO, InternetAddress.parse(InternetAddress.parse(correo));
+                        message.setContent(descripcion, "text/html;charset=utf-8");
+                        Transport.send(message);
+                        Toast.makeText(getApplicationContext(), "Message sent", Toast.LENGTH_LONG).show();
+                    }
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
     @Override
@@ -69,42 +119,6 @@ public class Contactar extends AppCompatActivity implements View.OnClickListener
 
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        nombre = etNombre.getText().toString();
-        correo = etCorreo.getText().toString();
-        descripcion = etDescripcion.getText().toString();
-
-        miCorreo = "androidcurso@gmail.com";
-        miClave  = "androidcurso";
-
-        Properties properties = new Properties();
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.socketFactory.port", "465");
-        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.port", "465");
-
-        try {
-            session = Session.getDefaultInstance(properties, new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(miCorreo, miClave);
-                }
-            });
-
-            if(session != null){
-
-                Message.message
-            }
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
     }
 
 }
